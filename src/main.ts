7139ec81ca06070c9af85b7ea04a775132d707d0
Kyunghwa <kyunghwa.oh@solytic.com>
2021-03-15 09:21:52 +0100

    setup apollo and graphql with webpack configs

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { ApolloServer } from 'apollo-server';
+
+import { environment } from './environment';
+import * as typeDefs from './type-defs.graphql';
+
+const server = new ApolloServer({
+  typeDefs,
+  introspection: environment.apollo.introspection,
+  mocks: true, // TODO: Remove in PROD.
+  mockEntireSchema: false, // TODO: Remove in PROD.
+  playground: environment.apollo.playground,
+});
+
+server
+  .listen(environment.port)
+  .then(({ url }) => console.log(`Server ready at ${url}. `));
+
+if (module.hot) {
+  module.hot.accept();
+  module.hot.dispose(() => server.stop());
+}
